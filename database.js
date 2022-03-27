6340a27cbcb951e9b6616c4e68988356a19cb2a1
keitannunes <keitannunes@gmail.com>
2022-03-27 14:15:52 -0400

    commented database.js

--- database.js
+++ database.js
@@ -1,42 +1,44 @@
+/*
+EVERYTHING DB RELATED
+*/
 const { createClient } = require("@supabase/supabase-js");
 const functions = require('./functions.js')
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
-
-async function signedUp(id) {
+async function signedUp(id) { //checks if user is signed up
     const { data } = await supabase
         .from('balance')
         .select('discordid')
         .eq('discordid', id)
     return typeof data[0] !== "undefined"
 }
-async function getBalance(id){
+async function getBalance(id){ //returns balance
     const { data } = await supabase
                     .from('balance')
                     .select('value')
                     .eq('discordid', id)
     return data[0].value
 }
-async function deleteUser(id){
+async function deleteUser(id){ //deletes user
 await supabase
                         .from('balance')
                         .delete()
                         .eq('discordid', id)
 }
-async function setBalance(id,value){
+async function setBalance(id,value){//sets user balance
     value = functions.roundCents(value);
     await supabase
                 .from('balance')
                 .update({ value: value })
                 .eq('discordid', id)
 }
-async function signUp(id,username){
+async function signUp(id,username){ //signs user up
     await supabase
                 .from('balance')
                 .insert([
                     { discordid: id, username: username },
                 ])
 }
-async function getAll(){
+async function getAll(){ //gets all users
     await supabase
     const {data} = await supabase
     .from('balance')
